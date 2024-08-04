825fc58c576125f40918093a412d0cfdd7664217
MykolaMazhula <mykm3ua@gmail.com>
2024-08-04 19:52:57 +0200

    remove redundant imports

--- train_station_api/train_station/models.py
+++ train_station_api/train_station/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User, AbstractUser
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.db.models import UniqueConstraint, CheckConstraint, Q
 
 
 class Journey(models.Model):
