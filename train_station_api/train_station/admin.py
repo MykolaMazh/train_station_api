1d9115b6fa4bab11e61173d2be282bf09c78a62a
MykolaMazhula <mykm3ua@gmail.com>
2024-09-15 20:31:59 +0200

    Add inline displaying for Route model(IntermediateStation, Journey)

--- train_station_api/train_station/admin.py
+++ train_station_api/train_station/admin.py
@@ -13,8 +13,25 @@ from train_station.models import (
 )
 
 
+class JourneyIncluded(admin.StackedInline):
+    save_as = True
+    list_display = ["route", "departure_time", "arrival_time"]
+    ordering = ["departure_time"]
+    model = Journey
+    extra = 0
+
+
 class TicketAdmin(admin.ModelAdmin):
     save_as = True
+
+
+class IntermediateStationIncluded(admin.StackedInline):
+    model = IntermediateStation
+    extra = 0
+
+
+class RouteAdmin(admin.ModelAdmin):
+    inlines = [IntermediateStationIncluded, JourneyIncluded]
 
 
 admin.site.register(Train)
@@ -24,5 +41,5 @@ admin.site.register(TrainType)
 admin.site.register(Ticket, TicketAdmin)
 admin.site.register(Station)
 admin.site.register(Order)
-admin.site.register(Route)
+admin.site.register(Route, RouteAdmin)
 admin.site.register(IntermediateStation)
