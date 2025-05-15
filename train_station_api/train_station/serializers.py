13de2624088465fc4974011fecb3daf82be85096
MykolaMazhula <mykm3ua@gmail.com>
2025-05-15 10:51:43 +0200

    Add Route serializers

--- train_station_api/train_station/serializers.py
+++ train_station_api/train_station/serializers.py
@@ -1,13 +1,22 @@
-from rest_framework import serializers
+from rest_framework import serializers, generics
 from rest_framework.relations import PrimaryKeyRelatedField
 
-from .models import Journey, CrewMember, Station, Train, TrainType, Route, IntermediateStation
+from .models import (
+    Journey,
+    CrewMember,
+    Station,
+    Train,
+    TrainType,
+    Route,
+    RouteStation,
+)
 
 
 class CrewMemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = CrewMember
         fields = ["id", "first_name", "last_name"]
+
 
 class StationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,7 +33,9 @@ class TrainListSerializer(serializers.ModelSerializer):
 
 
 class TrainSerializer(TrainListSerializer):
-    type = serializers.PrimaryKeyRelatedField(queryset=TrainType.objects.all(), source="_type")
+    type = serializers.PrimaryKeyRelatedField(
+        queryset=TrainType.objects.all(), source="_type"
+    )
 
 
 class TrainTypeSerializer(serializers.ModelSerializer):
@@ -33,37 +44,111 @@ class TrainTypeSerializer(serializers.ModelSerializer):
         fields = ["id", "name"]
 
 
+class RouteStationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = RouteStation
+        fields = [
+            "id",
+            "station",
+            "route_ordinal_station_number",
+            "route_distance_already_passed_km",
+        ]
+
+
+class RouteStationListSerializer(RouteStationSerializer):
+    station = serializers.CharField()
+
+
+class JourneySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Journey
+        fields = "__all__"
+
+
 class RouteSerializer(serializers.ModelSerializer):
+    route_stations = RouteStationSerializer(many=True)
+
     class Meta:
         model = Route
-        fields = ["id", "source", "destination", "distance"]
+        fields = ["id", "source", "destination", "distance", "route_stations"]
 
+    def create(self, validated_data):
+        route_stations_data = validated_data.pop("route_stations")
+        print(route_stations_data)
+        route = Route.objects.create(**validated_data)
+        for route_station in route_stations_data:
+            RouteStation.objects.create(route=route, **route_station)
+        return route
 
-class RouteRetrieveSerializer(RouteSerializer):
-    source = StationSerializer()
-    destination = StationSerializer()
+    def update(self, instance, validated_data):
+        route_stations_data = validated_data.pop("route_stations", None)
 
-class IntermediateStationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = IntermediateStation
-        fields = ["id", "name", "departure_list", "arrival_list"]
+        # Update the Route fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        if route_stations_data is not None:
+            # Clear and recreate route stations
+            instance.route_stations.all().delete()
+            for station_data in route_stations_data:
+                RouteStation.objects.create(route=instance, **station_data)
+
+        return instance
+
 
 class RouteRetrieveSerializer(serializers.ModelSerializer):
     source = StationSerializer()
     destination = StationSerializer()
-    intermediate_stations = IntermediateStationSerializer(many=True, source="route_intermediate_stations")
+    route_stations = RouteStationListSerializer(many=True)
+    journeys = serializers.SerializerMethodField()
 
     class Meta:
         model = Route
-        fields = ["id", "source", "destination", "distance", "intermediate_stations"]
+        fields = [
+            "id",
+            "source",
+            "destination",
+            "distance",
+            "route_stations",
+            "journeys",
+        ]
+
+    def get_journeys(self, obj):
+        journeys = obj.journeys.all().order_by("departure_time")
+        return [
+            (
+                f"Departs at {j.departure_time.strftime("%H:%M")} - Arrives at {j.arrival_time.strftime("%H:%M")}"
+            )
+            for j in journeys
+        ]
 
 
-class RouteListSerializer(RouteSerializer):
+class RouteListSerializer(serializers.ModelSerializer):
+    route_stations = serializers.SerializerMethodField()
     source = serializers.CharField()
     destination = serializers.CharField()
+    journeys = serializers.IntegerField(
+        source="journeys.count", read_only=True
+    )
 
+    class Meta:
+        model = Route
+        fields = [
+            "id",
+            "source",
+            "destination",
+            "distance",
+            "route_stations",
+            "journeys",
+        ]
 
-
+    def get_route_stations(self, obj):
+        route_stations = obj.route_stations.select_related("station").order_by(
+            "route_ordinal_station_number"
+        )
+        return [rs.station.name for rs in route_stations]
 
 
 
