bdb2aca3843d13c6a5928c5ea1147e717f79e5d7
MykolaMazhula <mykm3ua@gmail.com>
2025-06-02 12:11:26 +0200

    Add endpoint "me"

--- train_station_api/user/urls.py
+++ train_station_api/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, RetrieveUpdateUserView
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="register_user")
+    path("register/", CreateUserView.as_view(), name="register_user"),
+    path("me/", RetrieveUpdateUserView.as_view(), name="me"),
 ]
 
 app_name = "user"
--- train_station_api/user/views.py
+++ train_station_api/user/views.py
@@ -1,7 +1,20 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class RetrieveUpdateUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+
+3
